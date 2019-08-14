c7c78e92746648e8923c236232e11fcc4de5309c
sjana <Sandipan.Jana@hyland.com>
2019-08-15 00:10:38 +0530

    Add WebDriver Wait instead of thread.sleep

--- src/main/java/SignInTest.java
+++ src/main/java/SignInTest.java
@@ -1,7 +1,10 @@
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,7 +18,9 @@ public class SignInTest {
         setDriverPath();
 
         driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
+        //waitFor(2000);
+        
+        waitFor(10,driver.findElement(By.linkText("Your trips")));
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
@@ -27,12 +32,14 @@ public class SignInTest {
         driver.quit();
     }
 
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+    private void waitFor(int durationInSeconds , WebElement elem) {
+		/*
+		 * try { Thread.sleep(durationInMilliSeconds); } catch (InterruptedException e)
+		 * { e.printStackTrace(); //To change body of catch statement use File |
+		 * Settings | File Templates. }
+		 */
+    	WebDriverWait wait = new WebDriverWait (driver, 15);
+    	wait.until(ExpectedConditions.elementToBeClickable(elem));
     }
 
     private void setDriverPath() {
